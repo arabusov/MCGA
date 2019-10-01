baa9bafd8f1b87a0b31a8b9309871faea9c67bbc
Andrei Rabusov <a.rabusov@tum.de>
2019-10-01 10:07:57 +0200

    FAT12: start to think about

--- mbr/fat12.asm
+++ mbr/fat12.asm
@@ -1,7 +1,9 @@
 %include "fat12.inc"
 %include "bl.inc"
 ;FAT1
-        times   NBYTEPSEC*FATSIZE db 0
+fat1:       db      0xf0, 0xff, 0xff
+actual_fat_size equ $ - fat1
+        times   NBYTEPSEC*FATSIZE-actual_fat_size db 0
 ;FAT2
         times   NBYTEPSEC*FATSIZE db 0
 ;ROOT dir
