2c0aedaf51d5e8613b7b987ad5548aa78c1e2399
Andrei Rabusov <a.rabusov@tum.de>
2019-10-01 11:16:32 +0200

    wrong commit

--- mbr/floppy.asm
+++ mbr/floppy.asm
@@ -1,9 +1,7 @@
 %include "mbr.inc"
 %include "fat12.inc"
 %include "bl.inc"
-%ifndef FLOPPY_TYPE
-    %define FLOPPY_TYPE 1200
-%endif
+%include "floppy.inc"
 %define FLOPPY_NSEC FLOPPY_TYPE*1024/NBYTEPSEC 
 %define MBRFATBL_NSEC (1+2*FATSIZE+ROOTSIZE+BLNSEC)
 %define REMINDER_FLOPPY FLOPPY_NSEC - MBRFATBL_NSEC
