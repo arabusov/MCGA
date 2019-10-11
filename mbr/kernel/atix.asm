be6d2931abd961ca5cc8e186b7490e0b46929f3f
Andrei Rabusov <a.rabusov@tum.de>
2019-10-11 20:51:16 +0200

    More comments to ATIX init code

--- mbr/kernel/atix.asm
+++ mbr/kernel/atix.asm
@@ -5,12 +5,25 @@ cpu 286
 %include "fat12.inc"
 section .text
 org ATIX_OFFSET
+;----------------------------------------------------------------------------;
+;                               CODE SEGMENT                                 ;
+;                                                                            ;
+;----------------------------------------------------------------------------;
+
 start:
+
+;---------------------------------------;
+;        Initialization procedure       ;
+;                                       ;
+; The kernel is in real mode.           ;
+; Initialize segment registers:         ;
+;---------------------------------------;
+
             mov         ax, ATIX_SEG    ; Set all segment registers
             mov         ds, ax
             mov         es, ax
             mov         ss, ax
-            mov         sp, 0xffff
+            mov         sp, ATIX_STACK_OFFSET
 
             call        clrscr          ; Clear screen and print a "hello"
                                         ; message.
@@ -62,20 +75,20 @@ clrscr:
 ;----------------------------------------------------------------------------;
 
 println:
-            push    ds
-            push    es
+            push        ds
+            push        es
             pusha
-            mov     ax, ATIX_SEG
-            mov     ds, ax
-            mov     es, ax
-            mov bx, [crsps]
-            call    mvcurs
-            shl bx,1
-            call    print
-            call    newline
+            mov         ax, ATIX_SEG
+            mov         ds, ax
+            mov         es, ax
+            mov         bx, [crsps]
+            call        mvcurs
+            shl         bx,1
+            call        print
+            call        newline
             popa
-            pop     es
-            pop     ds
+            pop         es
+            pop         ds
             ret
 
 ;----------------------------------------------------------------------------;
@@ -88,90 +101,89 @@ print:
             push        ds
             push        es
             mov         ax, ATIX_SEG
-            mov     es, ax
-            cmp     cx,0
-            jz      endprn
-            mov     bx,[es:crsps]
-            shl     bx,1
-            mov ax, SCRSEG
-            mov ds, ax
-            mov ah, 0x02
+            mov         es, ax
+            cmp         cx,0
+            jz          endprn
+            mov         bx,[es:crsps]
+            shl         bx,1
+            mov         ax, SCRSEG
+            mov         ds, ax
+            mov         ah, 0x02
 get_symbol:
-            mov al, [es:bp]
-            cmp     al, 13 ;newline
-            jnz     check_10
-            push    bx
-            shr     bx,1
-            mov     [es:crsps],bx
-            call    mvcurs
-            call    newline
-            pop     bx
-            inc     bp
-            dec     cx
-            jz      endprn
-            jmp     get_symbol
+            mov         al, [es:bp]
+            cmp         al, 13 ;newline
+            jnz         check_10
+            push        bx
+            shr         bx,1
+            mov         [es:crsps],bx
+            call        mvcurs
+            call        newline
+            pop         bx
+            inc         bp
+            dec         cx
+            jz          endprn
+            jmp         get_symbol
 check_10:
-            cmp     al,10
-            jnz     contprint1
-            push    ax
-            mov     ax,bx
-            mov     bl,SCRNCL
-            div     bl
-            mov     bl,SCRNCL
-            mul     bl
-            mov     bx,ax
-            pop     ax
-            inc     bp
-            dec     cx
-            jz      endprn
-            jmp     get_symbol
+            cmp         al,10
+            jnz         contprint1
+            push        ax
+            mov         ax,bx
+            mov         bl,SCRNCL
+            div         bl
+            mov         bl,SCRNCL
+            mul         bl
+            mov         bx,ax
+            pop         ax
+            inc         bp
+            dec         cx
+            jz          endprn
+            jmp         get_symbol
 
 contprint1:
-            cmp bx, SCRNCL*SCRNRW*2
-            jb  contprint2
-            call    nl
-            mov bx,SCRNCL*(SCRNRW-1)*2
-            push bx
-            shr bx,1
-            call    mvcurs
-            pop bx
+            cmp         bx, SCRNCL*SCRNRW*2
+            jb          contprint2
+            call        nl
+            mov         bx,SCRNCL*(SCRNRW-1)*2
+            push        bx
+            shr         bx,1
+            call        mvcurs
+            pop         bx
 contprint2:
-            mov [bx], ax
-            add bx, 2
-            inc bp
-            loop get_symbol
+            mov         [bx], ax
+            add         bx, 2
+            inc         bp
+            loop        get_symbol
 
 endprn:
-            shr bx,1
-            call mvcurs
-            pop es
-            pop ds
+            shr         bx,1
+            call        mvcurs
+            pop         es
+            pop         ds
             popa
             ret
 
-
 mvcurs:
             pusha
-            push    ds
-            mov     ax, ATIX_SEG
-            mov     ds, ax
-            mov [crsps], bx ;save the position
-            mov dx, 0x03d4
-            mov al, 0x0f
-            out dx, al
+            push        ds
+            mov         ax, ATIX_SEG
+            mov         ds, ax
+            mov         [crsps], bx     ;save the position
+            mov         dx, 0x03d4
+            mov         al, 0x0f
+            out         dx, al
 
-            inc dl
-            mov al, bl
-            out dx, al
+            inc         dl
+            mov         al, bl
+            out         dx, al
 
-            dec dl
-            mov al, 0x0e
-            out dx, al
+            dec         dl
+            mov         al, 0x0e
+            out         dx, al
 
-            inc dl
-            mov al, bh
-            out dx, al
-            pop     ds
+            inc         dl
+            mov         al, bh
+            out         dx, al
+            pop         ds
             popa
             ret
 
@@ -179,57 +191,63 @@ mvcurs:
 ;newline subroutine
 nl:
             pusha
-            push    es
-            push    ds
+            push        es
+            push        ds
 
-            mov     ax, SCRSEG
-            mov     ds, ax
-            mov     es, ax
-            mov     di, 0x00
-            mov     si, SCRNCL*2
-            mov     cx, SCRNCL*(SCRNRW-1)*2
+            mov         ax, SCRSEG
+            mov         ds, ax
+            mov         es, ax
+            mov         di, 0x00
+            mov         si, SCRNCL*2
+            mov         cx, SCRNCL*(SCRNRW-1)*2
             cld
             rep
             movsb
-            mov     di, SCRNCL*(SCRNRW-1)*2
-            mov     cx, SCRNCL
-            mov     ax, 0x0200
+            mov         di, SCRNCL*(SCRNRW-1)*2
+            mov         cx, SCRNCL
+            mov         ax, 0x0200
 clearln:
-            mov     [di],ax
-            add     di,2
-            loop    clearln
+            mov         [di],ax
+            add         di,2
+            loop        clearln
 
-            pop ds
-            pop es
+            pop         ds
+            pop         es
             popa
             ret
 newline:
-        pusha
-        mov ax, [crsps]
-        mov bl,SCRNCL
-        div bl
-        cmp al, SCRNRW
-        je  cnl
-        add al,1
-        mov ah,0
-        mov bx,SCRNCL
-        mul bl
-        mov bx,ax
-        call    mvcurs
-        jmp endprint
+            pusha
+            mov         ax, [crsps]
+            mov         bl,SCRNCL
+            div         bl
+            cmp         al, SCRNRW
+            je          cnl
+            add         al,1
+            mov         ah,0
+            mov         bx,SCRNCL
+            mul         bl
+            mov         bx,ax
+            call        mvcurs
+            jmp         endprint
 
 cnl:
-        call    nl
-        mov bx,SCRNCL*(SCRNRW-1)
-        call    mvcurs
+            call        nl
+            mov         bx,SCRNCL*(SCRNRW-1)
+            call        mvcurs
 endprint:
-        popa
-        ret
+            popa
+            ret
+
+;----------------------------------------------------------------------------;
+;                               DATA SEGMENT                                 ;
+;                                                                            ;
+;----------------------------------------------------------------------------;
 
 atixmsg:    db          "ATIX loading..."
 atixmsgln   equ         $-atixmsg
 haltmsg     db          "ATIX: HALT PROCESSOR."
 haltmsgln   equ         $-haltmsg
 crsps:      db          0
-size    equ $-start
+size        equ         $-start
             times NBYTEPSEC*ATIX_NSEC-size db 0 ;empty sectors of the kernel
+
