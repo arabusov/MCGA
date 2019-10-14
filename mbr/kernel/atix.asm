d171c51edd6ed8fe8b8cd4894d0132ef26d0f4b6
Andrei Rabusov <a.rabusov@tum.de>
2019-10-14 10:43:44 +0200

    [ATIX] Disable interrupts, TSS

--- mbr/kernel/atix.asm
+++ mbr/kernel/atix.asm
@@ -5,7 +5,7 @@ cpu 286
 %include "fat12.inc"
 
 code_size   equ         end_code - start
-stack_size  equ         ATIX_STACK_OFFSET-code_size-data_size-ATIX_OFFSET
+stack_size  equ         ATIX_STACK_OFFSET-code_size-data_size-ATIX_OFFSET-tss_size
 
 section .text
 org ATIX_OFFSET
@@ -20,7 +20,39 @@ start:
 ;        Initialization procedure       ;
 ;                                       ;
 ; The kernel is in real mode.           ;
-; Initialize segment registers:         ;
+;                                       ;
+;---------------------------------------;
+
+;---------------------------------------;
+;           Mask interrupts             ;
+;---------------------------------------;
+
+            cli
+
+            mov         al, 0x11        ; Also, set up all two interrupt
+            out         0x20, al        ; controllers and mask all hardware
+                                        ; interrupts in addition to cli
+            out         0xa0, al        ; instruction.
+
+            mov         al, IRQ0        ; Remap hardware interrupts away
+            out         0x21, al        ; of i286 processor 
+            mov         al, IRQ8        ; interrupt "addresses".
+            out         0xa1, al
+
+            mov         al, 0x04        ; PIC1 is in "master" mode.
+            out         0x21, al
+            mov         al, 0x02        ; PIC2 is in "slave" mode.
+            out         0xa1, al
+
+            mov         al, 0x01        ; Initialization command word No. 4
+            out         0x21, al        
+            out         0xa1, al
+
+            mov         al, 0xff        ; And, finally, disable all interrupt
+            out         0x21, al        ; requests.
+
+;---------------------------------------;
+;        Initialize segment registers   ;
 ;---------------------------------------;
 
             mov         ax, ATIX_SEG    ; Set all segment registers
@@ -29,11 +61,24 @@ start:
             mov         ss, ax
             mov         sp, ATIX_STACK_OFFSET
 
+;---------------------------------------;
+;       Print a "hello" message         ;
+;---------------------------------------;
+
             call        clrscr          ; Clear screen and print a "hello"
                                         ; message.
             mov         bp, atixmsg
             mov         cx, atixmsgln
             call        println
+
+;---------------------------------------;
+;           Set A20 line on             ;
+;---------------------------------------;
+
+            mov         al, 0xdd
+            out         0x64, al
+
+            
 
 ;---------------------------------------;
 ;           Halt processor              ;
@@ -43,6 +88,7 @@ start:
             mov         cx, haltmsgln
             call        println
 
+            sti
 atix_halt:
             hlt
             jmp         atix_halt
@@ -317,7 +363,7 @@ end_code    equ         $
 ;                                                                            ;
 ;----------------------------------------------------------------------------;
 
-begin_data  equ         $
+begin_data:
 
 atixmsg:    db          "ATIX loading..."
 atixmsgln   equ         $-atixmsg
@@ -331,11 +377,12 @@ crsps:      db          0
 ;----------------------------------------------------------------------------;
 
 gdt:
-            DESCRIPTOR  0, 0, 0, 0
-.code_desc: DESCRIPTOR  ATIX_CODE_BASE, 0, code_size, CODE_ACC_BYTE
-.stack_desc:DESCRIPTOR  ATIX_STACK_BASE,0,stack_size,STACK_ACC_BYTE
-.data_desc: DESCRIPTOR  begin_data+ATIX_SEG*0x10,0,data_size,DATA_ACC_BYTE
-.video_desc:DESCRIPTOR  0xb800,0,SCRNRW*SCRNCL*2,DATA_ACC_BYTE
+            DESCRIPTOR  0, 0, 0
+.code_desc: DESCRIPTOR  ATIX_CODE_BASE, code_size, CODE_ACC_BYTE
+.stack_desc:DESCRIPTOR  ATIX_STACK_BASE, stack_size,STACK_ACC_BYTE
+.data_desc: DESCRIPTOR  begin_data+ATIX_SEG*0x10, data_size,DATA_ACC_BYTE
+.ktss_desc: DESCRIPTOR  kernel_tss+ATIX_SEG*0x10, kernel_tss_size, TSS_ACC_BYTE
+.video_desc:DESCRIPTOR  0xb8000, SCRNRW*SCRNCL*2,DATA_ACC_BYTE
 
 code_sel    equ         ((.code_desc -  gdt)/DESC_SIZE)*0x04
 data_sel    equ         ((.data_desc -  gdt)/DESC_SIZE)*0x04
@@ -349,10 +396,30 @@ video_sel   equ         ((.video_desc -  gdt)/DESC_SIZE)*0x04
 
 idt:
             %rep 0x100
-            DESCRIPTOR 0, 0, 0, 0
+            DESCRIPTOR 0, 0, 0
             %endrep
+end_data    equ         $
 
-data_size   equ         $-begin_data
+data_size   equ         end_data-begin_data
+
+begin_tss   equ         $
+kernel_tss  equ         $
+
+.back_link_sel:
+            resw        1
+.sp_cpl0:   resw        1
+.ss_cpl0:   resw        1
+.sp_cpl1:   resw        1
+.ss_cpl1:   resw        1
+.sp_cpl2:   resw        1
+.ss_cpl2:   resw        1
+.registers: resw        14
+.ldt_sel:   resw        1
+end_kernel_tss  equ     $
+kernel_tss_size equ     end_kernel_tss - kernel_tss
+end_tss     equ         $
+tss_size    equ         end_tss - begin_tss
+
 size        equ         $-start
             times NBYTEPSEC*ATIX_NSEC-size db 0 ;empty sectors of the kernel
 
